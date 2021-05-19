f7b99400bf329d399551d410d5518732cc7270b6
Franco Galeano <fgaleano1996@gmail.com>
2021-05-19 10:14:16 -0300

    FECHA 13

--- scripts/goleadores_xg.R
+++ scripts/goleadores_xg.R
@@ -19,6 +19,7 @@ goleadores <- read_sheet("https://docs.google.com/spreadsheets/d/1UDwZbQBGzpvTUw
   print()
 
 plot_xg <- goleadores %>% 
+  filter(fecha == 13) %>% 
   select(jugador, Actuales = goles, goles_sin_penales, Esperados = xg, pos) %>%
   print()
 
@@ -42,12 +43,19 @@ p1 <- plot_xg %>%
   geom_label_repel(size = 7, aes(label = jugador, family = "Encode Sans Wide"), vjust=1) +
   facet_grid(tipo ~ .)
 
+linea <- data.frame(x = c(3:9),y = c(3:9)) %>% 
+  as_tibble() %>% 
+  print()
+
 # Scatter ####
 p2 <- plot_xg %>% 
   ggplot(aes(Actuales, Esperados)) +
   geom_point(color = "white") +
   theme_linedraw() +
   labs(title = "") + 
+  geom_line(data = linea, aes(x = x, y = y), color = "blue") +
+  scale_y_continuous(breaks=c(2:8)) +
+  scale_x_continuous(breaks=c(4:8)) +
   theme(axis.title.y = element_text(size = 35),
         axis.title.x = element_text(size = 35),
         axis.text.y = element_text(size = 35),
@@ -77,11 +85,11 @@ p3 <- ggplot(mapping = aes(x = 0:1, y = 1)) +
 p4 <- gridExtra::grid.arrange(p1, p3, heights = c(.53, .10))
 
 #Exporto
-ggsave(p4, filename = "xg_fecha12.png",
+ggsave(p4, filename = "xg_fecha13.png",
        width = 12, height = 10, units = "in", path = "plots/")
 
 
 p4a <- gridExtra::grid.arrange(p2, p3, heights = c(.53, .10))
 
-ggsave(p4a, filename = "xg_scatter_fecha12.png",
+ggsave(p4a, filename = "xg_scatter_fecha13.png",
        width = 12, height = 10, units = "in", path = "plots/")
